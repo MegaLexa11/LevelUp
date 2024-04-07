d9a0b59f80e4ebe322743b4a155a7497f46ae33f
MegaLexa <kukh99@mail.ru>
2024-04-07 14:46:28 +0500

    Homework done, added .vs to gitignore file

---
+++ Homework/Homework 1.1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Homework_1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: ");
+            int num1 = int.Parse(Console.ReadLine() ?? string.Empty);
+            Console.Write("Enter second number: ");
+            int num2 = int.Parse(Console.ReadLine() ?? string.Empty);
+            int result = num1 + num2;
+
+            Console.WriteLine($"{num1} + {num2} = {result}");
+
+        }
+    }
+}
