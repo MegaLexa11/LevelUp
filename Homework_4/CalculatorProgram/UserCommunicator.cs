0b3a2405937d12971b1e581946824a7ae9a5cc05
MegaLexa <kukh99@mail.ru>
2024-04-17 20:27:38 +0500

    Homework 4 done
    Calculator program was done

---
+++ Homework_4/CalculatorProgram/UserCommunicator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProgram
+{
+    internal class UserCommunicator
+    {
+        public static void CalculatorGreeting()
+        {
+            Console.WriteLine("Welcome to simple expression calculator!");
+            Console.WriteLine("Available actions:");
+            Console.WriteLine("- Addiction (+);");
+            Console.WriteLine("- Subtraction (-);");
+            Console.WriteLine("- Multiplication (*);");
+            Console.WriteLine("- Division (+);");
+            Console.WriteLine("- Exponentiation (^);");
+            Console.WriteLine("- Cosinus();");
+            Console.WriteLine("- Sinus().");
+            Console.WriteLine("Expression example: 1+2 or cos(30)");
+            Console.WriteLine();
+        }
+
+        public static void CalculatorFinishing(double result) 
+        {
+            Console.WriteLine($"The result is {result}");
+        }
+
+        public static bool CalculatorContinuing()
+        {
+            Console.Write("Would you like to calculate again (y)?: ");
+            string oneMoreCalc = (Console.ReadLine() ?? string.Empty).ToLower();
+            bool IsOneMoreCalc = String.Compare(oneMoreCalc, "y") == 0;
+            return IsOneMoreCalc;
+        }
+    }
+}
