560433a8671f6f35d254c7da8a8b997841d32964
MegaLexa <kukh99@mail.ru>
2024-05-12 13:00:27 +0500

    Homework 10 done... probably

---
+++ Lesson10/PhoneNumberSearcher/PhoneNumberSearcher/PhoneNumberSearch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PhoneNumberSearcher
+{
+    internal class PhoneNumberSearch
+    {
+        public static MatchCollection Search(string strToSearch)
+        {
+            // Есть стойкое ощущение, что мог сделать что-то не то... Но, по идее, если системой предусмотрены методы работы подобные, то они должны быть наименее затратными?
+            // Хотя, с другой стороны, программа возвращает MatchCollection, что может потреблять больше памяти, чем массив строк - но, опять же, изобретать заново регулярки звучит, как что-то сомнительное
+            string phonePattern = @"\+[0-9]{1}-[0-9]{3}-[0-9]{3}-[0-9]{4}";
+            var stringResult = Regex.Matches(strToSearch, phonePattern);
+            return stringResult;
+        }
+    }
+}
---
+++ Lesson10/PhoneNumberSearcher/PhoneNumberSearcher/Program.cs
@@ -0,0 +1,7 @@
+﻿using PhoneNumberSearcher;
+
+var someStuff = PhoneNumberSearch.Search("+7-909-123-2345dsadsadsad+7-890-222-1355");
+foreach (var s in someStuff)
+{
+    Console.WriteLine(s);
+}
