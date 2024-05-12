25c7ebb028fae751c58479ad0847eaa30b3b359d
MegaLexa <kukh99@mail.ru>
2024-05-12 11:20:38 +0500

    Practice from lesson

---
+++ Lesson10/DuplicateSearcher/DuplicateSearcher/DuplicateSearch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuplicateSearcher
+{
+    internal static class DuplicateSearch
+    {
+        public static int Search(string baseStr, string duplicateStr)
+        {
+            int duplicateCount = 0;
+            int indexOfSubstr = 0;
+            int slicePos = 0;
+            while (true)
+            {
+                indexOfSubstr = baseStr.AsSpan()[slicePos..].IndexOf(duplicateStr);
+                if (indexOfSubstr != -1) 
+                {
+                    duplicateCount++;
+                    slicePos += indexOfSubstr + duplicateStr.Length;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return duplicateCount;
+        }
+
+    }
+}
