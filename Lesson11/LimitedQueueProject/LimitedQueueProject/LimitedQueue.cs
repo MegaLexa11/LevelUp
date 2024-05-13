223b0c2c9149c341a779caf7b8cd08bb9630a42f
MegaLexa <kukh99@mail.ru>
2024-05-13 18:53:23 +0500

    Homework 11 done

---
+++ Lesson11/LimitedQueueProject/LimitedQueueProject/LimitedQueue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LimitedQueueProject
+{
+    internal class LimitedQueue<T> : IEnumerable
+    {
+        private readonly Queue<T> _queue = new Queue<T>();
+
+        public int QueueLimit { get; }
+
+        public LimitedQueue(int limit)
+        {
+            QueueLimit = limit;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _queue.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Enqueue(T item)
+        {
+            if (_queue.Count >= QueueLimit)
+            {
+                throw new Exception($"Can't add any object! The limit of queue is {QueueLimit}.");
+            }
+            _queue.Enqueue(item);
+        }
+
+        public T Dequeue()
+        {
+             return _queue.Dequeue();
+        }
+    }
+}
---
+++ Lesson11/LimitedQueueProject/LimitedQueueProject/Program.cs
@@ -0,0 +1,10 @@
+﻿
+using LimitedQueueProject;
+
+LimitedQueue<int> customQueue = new LimitedQueue<int>(3);
+customQueue.Enqueue(1);
+customQueue.Enqueue(1);
+customQueue.Enqueue(1);
+customQueue.Dequeue();
+customQueue.Enqueue(1);
+customQueue.Enqueue(1);
