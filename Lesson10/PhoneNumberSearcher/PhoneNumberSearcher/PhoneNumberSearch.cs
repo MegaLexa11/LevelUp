cbb283a1172961725cd94d4c09d5093a8806483d
MegaLexa <kukh99@mail.ru>
2024-05-23 19:42:23 +0500

    Tried some useless stuff

--- Lesson10/PhoneNumberSearcher/PhoneNumberSearcher/PhoneNumberSearch.cs
+++ Lesson10/PhoneNumberSearcher/PhoneNumberSearcher/PhoneNumberSearch.cs
@@ -17,5 +17,21 @@ namespace PhoneNumberSearcher
             var stringResult = Regex.Matches(strToSearch, phonePattern);
             return stringResult;
         }
+
+        public static List<string> SearchOptimized(string strToSearch)
+        // Явно не то, что ожидалось от спанов, надо еще подумать
+        {
+            // Есть стойкое ощущение, что мог сделать что-то не то... Но, по идее, если системой предусмотрены методы работы подобные, то они должны быть наименее затратными?
+            // Хотя, с другой стороны, программа возвращает MatchCollection, что может потреблять больше памяти, чем массив строк - но, опять же, изобретать заново регулярки звучит, как что-то сомнительное
+            string phonePattern = @"\+[0-9]{1}-[0-9]{3}-[0-9]{3}-[0-9]{4}";
+            var phoneLength = 15;
+            var stringResult = Regex.EnumerateMatches(strToSearch, phonePattern);
+            var phones = new List<string>();
+            foreach (ValueMatch match in stringResult)
+            {
+                phones.Add((strToSearch.AsSpan().Slice(match.Index, phoneLength)).ToString());
+            }
+            return phones;
+        }
     }
 }
--- Lesson10/PhoneNumberSearcher/PhoneNumberSearcher/Program.cs
+++ Lesson10/PhoneNumberSearcher/PhoneNumberSearcher/Program.cs
@@ -1,7 +1,10 @@
-﻿using PhoneNumberSearcher;
+﻿using BenchmarkDotNet.Running;
+using PhoneNumberSearcher;
 
-var someStuff = PhoneNumberSearch.Search("+7-909-123-2345dsadsadsad+7-890-222-1355");
+/*var someStuff = PhoneNumberSearch.SearchOptimized("+7-909-123-2345dsadsadsad+7-890-222-1355");
 foreach (var s in someStuff)
 {
     Console.WriteLine(s);
-}
+}*/
+
+BenchmarkRunner.Run<PhoneNumberSearchBenchmark>();
